70841456cb3e9d72fa2a0260a8402c423e0be247
zhanglugen <13798486164@163.com>
2020-07-05 18:20:11 +0800

    add hr-service

---
+++ hr-service/22/Test.java
@@ -0,0 +1,15 @@
+package day01;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Test {
+	public static void main(String[] args) {
+		/*SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println(sdf.format(new Date()));*/
+		A ab=new B();
+		
+		System.out.println();
+		ab=new B();
+	}
+}
